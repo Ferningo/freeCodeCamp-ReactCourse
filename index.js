5cf220e39263b75543f492d244d4f7c2430b21b8
Jose <jose.mar.123@hotmail.com>
2023-05-09 22:14:08 -0600

    Submit latest excercise from the scrimba course

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const myElement = (
-  <div>
-    <h1>Behold, the power of React!</h1>
-    <ul>
-      <li>One</li>
-      <li>Two</li>
-      <li>Three</li>
-    </ul>
-  </div>
-);
+const MyComponent = () => {
+  return (
+    <>
+      <h1>Reasons why I'm excited about learning React</h1>
+      <ol>
+        <li>It's awesome</li>
+        <li>It's cool</li>
+        <li>It's interesting</li>
+        <li>It's enigmatic</li>
+      </ol>
+    </>
+  );
+};
 
-ReactDOM.render(myElement, document.getElementById("main-section"));
+const rootElement = document.getElementById("main-section");
+ReactDOM.render(<MyComponent />, rootElement);
